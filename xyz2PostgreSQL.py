035afac499814db0d3dca234351f3fe33fb29987
Wei Li <z5039792@ad.unsw.edu.au>
2020-09-24 22:38:54 +1000

    update postgre

--- xyz2PostgreSQL.py
+++ xyz2PostgreSQL.py
@@ -5,7 +5,6 @@
 xyz2PostgreSQL.py
 Read .xyz file and efficiently write into PostgreSQL in parallel.
 
-
 @date: Sep 24, 2020
 """
 
@@ -13,12 +12,10 @@ from __future__ import print_function
 from __future__ import division
 import os
 import sys
-import json
+import psycopg2
 import argparse
 import multiprocessing as mp
-from geojson import Point, MultiPoint
 from pyproj import CRS, Transformer
-from collections import OrderedDict
 from config import stopwatch
 import warnings
 warnings.filterwarnings('ignore')
@@ -33,9 +30,11 @@ parser.add_argument('-t', '--type', default='point', help='GeoJSON object type')
 parser.add_argument('-b', '--block', default=3000, help='size of block for multipoint')
 parser.add_argument('-c', '--collection', default='small_flat', help='collection in MongoDB')
 args = parser.parse_args(sys.argv[1:])
-if not os.path.exists(args.input):
-	sys.exit('Input file doesnot exist!!')
 
+# Check input arguments validation
+assert os.path.isfile(args.input), '%s not found' % args.input
+if not os.path.exists(args.splitDir):
+	os.mkdir(args.splitDir)
 
 
 def run(file_path, split_path):
